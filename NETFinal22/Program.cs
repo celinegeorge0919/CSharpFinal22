d59674ae8a186f6f5e12f28aaf3baa6cf74e7e2c
Celine <celinegeorge0919@gmail.com>
2020-08-15 10:15:42 -0400

    final test qn:22

--- NETFinal22/Program.cs
+++ NETFinal22/Program.cs
@@ -2,11 +2,53 @@
 
 namespace NETFinal22
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Sample sample = new Sample();
+      
+            Example eg = new Example();
+
+            eg.ExampleCall += sample.ExampleCallFunc;
+       
+            eg.createNotification("Show Notification ");
+
+            Console.ReadLine();
+
+        }
+    }
+
+    public class Sample
+    {
+        public void ExampleCallFunc(object sender, ExampleInfo e)
+        {
+            Console.WriteLine("Create Event: " + e.address);
+        }
+
+    }
+
+    public class Example
+    {
+        public event ExampleEvent ExampleCall;
+
+        public void createNotification(string msg)
+        {
+            if (ExampleCall != null)
+            {
+                ExampleCall(this, new ExampleInfo(msg));
+            }
+        }
+    }
+
+    public delegate void ExampleEvent(object sender, ExampleInfo e);
+
+    public class ExampleInfo : EventArgs
+    {
+        public string address { get; set; }
+        public ExampleInfo(string Address)
+        {
+            address = Address;
         }
     }
 }
